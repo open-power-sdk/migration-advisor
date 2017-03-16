b474f1bf88538d8a50c4d532537d6742ab4a78c1
Roberto Guimaraes Dutra De Oliveira <rdutra@br.ibm.com>
2017-03-16 16:08:49 -0300

    tools-ma: group problems by file in migration report
    Group problems of same category by file when printting the migration
    report.
    
    Change-Id: I79807bc7e247c17cb95e64306b50d5225362ddfd
    Signed-off-by: Roberto Oliveira <rdutra@linux.vnet.ibm.com>

--- ma/problem_reporter.py
+++ ma/problem_reporter.py
@@ -76,15 +76,28 @@ class ProblemReporter(object):
     @classmethod
     def print_problems(cls):
         """ Print all reported problems """
+        if not cls.problems:
+            print "\nNo migration reports found."
+            return
+
+        TAB = "   "
         cls.__print_logo()
         for problem_type, problems in cls.problems.items():
+            problems_dict = {}
+            # Group problems by file
+            for problem in problems:
+                name = problem.file_name
+                problems_dict[name] = problems_dict.get(name, []) + [problem]
+
             print "Problem type: " + problem_type
             print "Problem description: " + problems[0].problem_msg
-            for problem in problems:
-                print "   Name: " + problem.name
-                print "   File: " + problem.file_name
-                print "   Line: " + str(problem.line)
-                print ""
+            for file_name, problems in problems_dict.items():
+                print TAB + "File: " + file_name
+                for problem in problems:
+                    print (TAB * 2) + "Problem: " + problem.name
+                    print (TAB * 2) + "Line: " + str(problem.line)
+                    print ""
+            print ""
 
     @classmethod
     def get_problems(cls):
